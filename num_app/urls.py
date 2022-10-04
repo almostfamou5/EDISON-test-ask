1e38a31131ec82a3da312e814d4b86650f48d1a4
almostfamou5 <102470545+almostfamou5@users.noreply.github.com>
2022-10-04 16:19:04 +0700

    Delete urls.py

--- num_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.start_game, name='start'),
-    path('user_choice/', views.input_your_number, name='write_your_number'),
-    ]
